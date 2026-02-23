f43a3aaf1fd56ece89d72c0645d3223906f58778
roman9234 <roman_lesovoy@mail.ru>
2026-02-23 19:39:14 +0300

    working on labs

---
+++ stat_3_course/old_projects/prakt_4/main.py
@@ -0,0 +1,149 @@
+import math
+from random import randint
+import matplotlib.pyplot as plt
+
+from math import sqrt, pi, exp
+from typing import List
+
+import numpy as np
+
+
+# T.normal(_n) = 2nd(k1) + 2nd(k2) + 2nd(k3) + 2nd(k4)
+# _n  =10 20 50 100 200 10^3
+
+# строим гистограмму мин-макс. делим на 10 интервалов
+# узнаём как изменется характер чисел в зависимости от величин
+# строим кривую
+
+# 1/S * sqrt(2pi)
+
+# k1 k2 k3 будут выданы каждому
+
+
+def how_often(_list: List, _max=13, _min=0, _intervals_amount=13):
+    # _intervals = [x + 1 for x in range(len(_list))]
+    delta = _max / _intervals_amount
+    _frequencies_borders = [x * delta for x in range(0, _intervals_amount + 1)]
+    _frequencies = [0 for x in range(_intervals_amount + 1)]
+    for x in _list:
+        _i = 0
+        while True:
+            if x <= _frequencies_borders[_i]:
+                _frequencies[_i] += 1
+                break
+            _i += 1
+
+    for _i in range(len(_frequencies)):
+        _frequencies[_i] = _frequencies[_i] / (len(_list))
+
+    for _i in range(len(_frequencies_borders)):
+        _frequencies_borders[_i] -= delta / 2
+
+    return _frequencies_borders, _frequencies
+
+
+class ListEvaluator:
+    @staticmethod
+    def math_expectance(_random_values: List):
+        return sum(_random_values) / len(_random_values)
+
+    @staticmethod
+    def dispersion(_random_values: List):
+        # массив элементов, из каждого числа вычитаем мат ожидание и возводим в квадрат, а сумму элементов делим на количество числе -1
+        # Должно быть 0.0833333333
+        # только для
+        _M = ListEvaluator.math_expectance(_random_values)
+        _diviations = []
+        for _i in _random_values:
+            _diviations.append((_i - _M) ** 2)
+        return sum(_diviations) / (len(_diviations) - 1)
+
+    @staticmethod
+    def periodic_evaluator(_random_values: List):
+        for _n in range(len(_random_values)):
+            if _random_values[0] == _random_values[_n] and _n != 0:
+                print(f"Оченка периодичности - числа повторяются примерно раз в {_n} значений")
+                return _n
+        else:
+            print(f"Значения повторяются реже, чем раз в {len(_random_values)}")
+
+    @staticmethod
+    def sredne_kvadr(_random_values: List):
+        return math.sqrt(ListEvaluator.dispersion(_random_values))
+
+    @staticmethod
+    def frequency_evaluator(_a: int, _b: int, _interval_count: int, _random_values: List):
+        result = [x for x in range(10)]
+        return result
+
+
+print("\nЗадание 1")
+
+
+# создаём функцию-генератор
+
+
+class Generator:
+    @staticmethod
+    def get_t(k1=2, k2=1, k3=7, k4=3):
+        return randint(0, k1) + randint(0, k2) + randint(0, k3) + randint(0, k4)
+
+    @staticmethod
+    def get_list(_n):
+        return list(Generator.get_t() for _x in range(_n))
+
+
+print(Generator.get_list(5))
+
+print("\nЗадание 2-3")
+# генерируем списки разной длины
+n_values = [10, 20, 50, 100, 200, 500, 10 ** 3]
+for n in n_values:
+    print(f"\nn = {n}")
+    generated_list = Generator.get_list(n)
+    print(f"Мат ожидание равно {round(ListEvaluator.math_expectance(generated_list), 2)}")
+    print(f"Дисперсия равна {round(ListEvaluator.dispersion(generated_list), 2)}")
+    print(f"Среднеквадратическое отклонение {round(ListEvaluator.sredne_kvadr(generated_list), 2)}")
+    print(Generator.get_list(n))
+
+print("\nЗадание 4-9")
+
+rnd_values_list = Generator.get_list(1000)
+
+frequencies_borders, frequencies = how_often(rnd_values_list, _intervals_amount=9)
+
+print(sum(frequencies))
+# -------- Реальные M и D --------
+# среднеквадр. отклонение
+q = ListEvaluator.sredne_kvadr(rnd_values_list)
+# мат ожидание
+m = ListEvaluator.math_expectance(rnd_values_list)
+# Функция распределения
+expected = [(1 / (q * sqrt(2 * pi))) * exp(
+    -((x / 10 - m) ** 2) / (2 * (q ** 2))
+) for x in range(0, 140)]
+
+plt.plot([x / 10 for x in range(140)], expected, color="r")
+
+# -------- Теоретические M и D --------
+k1 = 2
+k2 = 1
+k3 = 7
+k4 = 3
+# среднеквадр. отклонение
+q = math.sqrt(
+    (((k1 - 0) ** 2) / 12 + ((k2 - 0) ** 2) / 12 + ((k3 - 0) ** 2) / 12 + ((k4 - 0) ** 2) / 12)
+)
+
+# мат ожидание
+m = (k1 + k2 + k3 + k4) / 2
+# Функция распределения
+expected = [(1 / (q * sqrt(2 * pi))) * exp(
+    -((x / 10 - m) ** 2) / (2 * (q ** 2))
+) for x in range(0, 140)]
+
+plt.plot([x / 10 for x in range(140)], expected, color="g")
+plt.xticks(frequencies_borders)
+plt.bar(frequencies_borders, frequencies)
+
+plt.show()
---
+++ stat_3_course/old_projects/prakt_4_1/main.py
@@ -0,0 +1,139 @@
+# Используем другие последовательности случайных числе
+from math import sqrt, exp, pi
+from random import randint, random
+from typing import List
+
+from ListEvaluator import ListEvaluator
+
+
+# NA(n,p)
+# Функция гарантирует частоты
+# p = 0.5
+
+# Эта программа генерирует последовательности, которые имеют другой характер распределения
+
+# Надо выделить некое Nкритическое число, для того чтобы уйти от этого начального участка
+# Оно задаётся в виде начальных данных - 50 100 200 - значения для Nкритического
+
+# На этом значении прогоняем случайные числа 10 20 50
+
+# Линейный закон
+
+# От Amin до Amax
+
+def how_often(_list: List, _max=1, _min=0, _intervals_amount=10):
+    # _intervals = [x + 1 for x in range(len(_list))]
+    delta = (_max-_min) / _intervals_amount
+    _frequencies_borders = [x * delta for x in range(1, _intervals_amount + 1)]
+    _frequencies = [0 for x in range(1,_intervals_amount + 1)]
+    for x in _list:
+        _i = 0
+        while True:
+            if x <= _frequencies_borders[_i]:
+                _frequencies[_i] += 1
+                break
+            _i += 1
+
+    for _i in range(len(_frequencies)):
+        _frequencies[_i] = _frequencies[_i] / (len(_list))
+
+    for _i in range(len(_frequencies_borders)):
+        _frequencies_borders[_i] -= delta / 2
+
+    return _frequencies_borders, _frequencies
+
+
+
+
+
+import matplotlib.pyplot as plt
+less_than_p_values_list = []
+
+n_values = [50,100,200]
+
+
+
+
+p = 0.5
+# p = 0.1
+n = 1000
+m = 0
+
+
+
+random_values_list = []
+for i in range(1,n+1):
+    ui = random()
+    if ui < p:
+        m+=1
+    less_than_p_value = m / i
+    random_values_list.append(ui)
+    less_than_p_values_list.append(less_than_p_value)
+
+    if i in n_values:
+    # if i == 50:
+
+        # -------- M и D --------
+        # среднеквадр. отклонение
+        average_mistake = ListEvaluator.sredne_kvadr(random_values_list)
+        print(f"Среднекв отклонение = {average_mistake}")
+
+        # мат ожидание
+        math_expectance = ListEvaluator.math_expectance(random_values_list)
+        print(f"Мат ожидание = {math_expectance}")
+
+        # Функция распределения
+        expected = [(1 / (average_mistake * sqrt(2 * pi))) * exp(
+            -((x / 10 - math_expectance) ** 2) / (2 * (average_mistake ** 2))
+        ) / 10 for x in range(0, 110)]
+
+
+        fig, ax = plt.subplots(1, 2, width_ratios=(3.0,4.0))
+        plot1 = ax[0]
+        plot2 = ax[1]
+
+        plot1.set_ylim(0,1)
+        plot2.set_xlim(0,1)
+        plot2.set_ylim(0,0.4)
+
+        plot1.set_yticks([x/10 for x in range(10)])
+        plot1.plot(list(range(i)), [0.5 for x in range(i)], color="y")
+        plot1.plot(list(range(i)), less_than_p_values_list)
+
+        frequencies_borders, frequencies = how_often(random_values_list)
+
+        # Сумма плозадей частот
+        print(sum(frequencies))
+
+        plot2.set_xticks(frequencies_borders)
+        plot2.plot([x / 10 for x in range(110)], expected, color="r")
+        plot2.bar(frequencies_borders, frequencies,width=0.1)
+        plot2.plot([x / 10 for x in range(110)], expected, color="r")
+
+        plt.show()
+
+
+
+
+
+# plt.plot(list(range(n)), less_than_p_values_list )
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
